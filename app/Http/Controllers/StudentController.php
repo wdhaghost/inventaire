fb7464a24fb9090f7001bdbec87ce164ea4d2242
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-10-20 14:52:21 +0200

    Student store method

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        // List all resources
+    }
+
+    public function create()
+    {
+        // Show the create resource form
+    }
+
+    public function store(Request $request)
+    {
+        // Store a new resource in the database
+        $student = new Student;
+        $student->lastname=$request->lastname;
+        $student->firstname=$request->firstname;
+        $student->mail=$request->mail;
+        $student->save();
+        return response(status:201);
+    }
+
+    public function show($id)
+    {
+        // Display a specific resource
+    }
+
+    public function edit($id)
+    {
+        // Show the edit resource form
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Update a specific resource in the database
+    }
+
+    public function destroy($id)
+    {
+        // Delete a specific resource
+    }
+}
